9f854e27d62598b533e527aa8242e2a79264cd6d
Brian Capps <Brian.Capps@nytimes.com>
2014-12-22 15:34:54 -0500

    Adds a collection view. Initial commit.

---
+++ Recommendations/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Recommendations
+//
+//  Created by Brian Capps on 12/22/14.
+//  Copyright (c) 2014 NYTimes. All rights reserved.
+//
+
+@import UIKit;
+
+@interface ViewController : UICollectionViewController
+
+
+@end
+
---
+++ Recommendations/ViewController.m
@@ -0,0 +1,41 @@
+//
+//  ViewController.m
+//  Recommendations
+//
+//  Created by Brian Capps on 12/22/14.
+//  Copyright (c) 2014 NYTimes. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UICollectionViewFlowLayout *flowLayout;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.collectionView.alwaysBounceVertical = YES;
+    
+    self.flowLayout.minimumInteritemSpacing = 10;
+    
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+#pragma mark - UICollectionViewDataSource
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return 3;
+}
+
+// The cell that is returned must be retrieved from a call to -dequeueReusableCellWithReuseIdentifier:forIndexPath:
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    return [self.collectionView dequeueReusableCellWithReuseIdentifier:@"albumCell" forIndexPath:indexPath];
+}
+
+
+@end
